7c0744f1e70085648f1bf55e959ac868616555dd
Chock <chock@rosystain.com>
2025-07-30 21:03:48 +0800

    fix

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# AstrBot URL 解析插件
+# Mafuyu Commands
 
-这是一个私人插件，旨在为 Bot 扩展迎合个人需求的日常功能。
+这是一个私人向插件，旨在为 Bot 扩展各种功能以迎合各种日常需求。
 
 # 功能
-1. /url - 解析 url 并根据 url 的内容自动执行对应动作
-   1. 解析到来自 e-hentai 的 url 时，将 url 发送到 http://10.0.0.3:5001/api/download?url={url}
+## Hentai Assistant API
+监听群聊和私聊，对来自 E-Hentai 的画廊链接作出回应。
+- 群聊中，特权用户可以对指定消息回复 download 或者 dl 触发动作。
--- main.py
+++ main.py
@@ -1,37 +1,51 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
-from astrbot.api import logger
+from astrbot.api import logger, AstrBotConfig
 import re
 import aiohttp
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+@register("MafuyuCommands", "Chock", "为真冬ちゃん扩展一些功能", "1.0.0")
 class MyPlugin(Star):
-    def __init__(self, context: Context):
+    def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
+        self.config = config
+        self.api_url = f"{self.config.get('hentai_assistant_api_url')}"
+        logger.info(f"MafuyuCommands 初始化完毕")
+        logger.info(f"Hentai Assistant API URL: {self.api_url}")
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
                 
     async def send_to_hentai_assistant(self, url):
-        api_url = f"http://10.0.0.3:5001/api/download?url={url}"
+        download_api = f"{self.api_url}/api/download?url={url}"
         async with aiohttp.ClientSession() as session:
-            async with session.get(api_url) as response:
+            async with session.get(download_api) as response:
                 if response.status == 202:
                     data = await response.json()
                     return True, data
                 else:
-                    logger.error(f"Failed to fetch data from {api_url}, status code: {response.status}")
+                    logger.error(f"Failed to fetch data from {download_api}, status code: {response.status}")
                     return False, response.status
-
-    @filter.event_message_type(filter.EventMessageType.PRIVATE_MESSAGE)
-    async def on_private_message(self, event: AstrMessageEvent):
+    
+    @filter.event_message_type(filter.EventMessageType.ALL)
+    async def on_all_message(self, event: AstrMessageEvent):
+        message_obj= event.message_obj.message # AstrBot 解析出来的消息链内容
+        logger.info(message_obj)
         message_str = event.message_str # 获取消息的纯文本内容
-        pattern = r"^https://e.hentai\.org/g/\d+/[a-zA-Z0-9]+/$"
-        if re.fullmatch(pattern, message_str):
-            result = await self.send_to_hentai_assistant(url=message_str.strip(" "))
+        if message_str == "dl" or message_str == "download":
+            for item in message_obj[0].chain:
+                if item == 'Plain':
+                    message_str = item.text
+        pattern = r"https://e.hentai\.org/g/\d+/[a-zA-Z0-9]+/"
+        match = re.search(pattern, message_str)
+        if match: # 直接处理包含画廊链接的消息
+            result = await self.send_to_hentai_assistant(url=match.group(0))
             if result[0] == True:
                 yield event.plain_result(f"收到了 {result[1]['message']} 这样的信息，看起来推送成功了呢。") # 调用异步方法发送请求到 hentai 助手
             elif result[0] == False:
                 yield event.plain_result(f"返回了 {result[1]} 这样的错误代码，看起来推送失败了呢。")
+
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+
+
